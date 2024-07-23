36654058cdaa7b049818246ba139b8a43a199f7e
Azaz <azaz.ali1706@gmail.com>
2024-07-24 01:45:07 +0530

    deployment the project

--- server/server.js
+++ server/server.js
@@ -1,22 +1,31 @@
-const express = require('express');
-const db = require('./config/db.js'); // Ensure this is set up to connect to your database
+const express = require("express");
+const db = require("./config/db.js"); // Ensure this is set up to connect to your database
 const app = express();
-require('dotenv').config();
+require("dotenv").config();
 
 const PORT = process.env.PORT || 8000;
 
 app.use(express.json());
 
-const userRoute = require('./routes/userRoutes.js');
-app.use('/api/users', userRoute);
+const userRoute = require("./routes/userRoutes.js");
+app.use("/api/users", userRoute);
 
-const inventoryRoute = require('./routes/inventoryRoutes.js');
-app.use('/api/inventory', inventoryRoute);
+const inventoryRoute = require("./routes/inventoryRoutes.js");
+app.use("/api/inventory", inventoryRoute);
 
-const dashboardRoute = require('./routes/dashboardRoute.js'); 
-app.use('/api/dashboard', dashboardRoute);
+const dashboardRoute = require("./routes/dashboardRoute.js");
+app.use("/api/dashboard", dashboardRoute);
 
+// deployment config
+const path = require("path");
+__dirname = path.resolve();
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+}
 app.listen(PORT, () => {
-    console.log(`Server listening at port: ${PORT}`);
+  console.log(`Server listening at port: ${PORT}`);
 });
